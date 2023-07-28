a76de1269115dac74c7807350de84f584d77dbec
sunajung <jungsuna99@gmail.com>
2023-07-28 23:10:16 +0900

    Add google login
    -Add google login main code with login button
    -Add necessary package.
    *Nuget
    *Json
    *Some other things

--- Assets/Scripts/Login/LoginButtonController.cs
+++ Assets/Scripts/Login/LoginButtonController.cs
@@ -1,18 +1,19 @@
-using System.Collections;
-using System.Collections.Generic;
-using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-using UnityEngine.UI;
-
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Security.Cryptography;
+using System;
+using System.IO;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+using System.Threading.Tasks;
 
 public class LoginButtonController : MonoBehaviour
 {
     // Start is called before the first frame update
-    public GameObject idText;
-    public GameObject pwText;
     void Start() {
-
     }
 
     // Update is called once per frame
@@ -20,14 +21,236 @@ public class LoginButtonController : MonoBehaviour
     {
         
     }
-    
-    public void login() {
+
+    public void Login() {
         bool success = true;
 
-		if (success) {
+        if (success) {
             SceneManager.LoadScene("CharacterSelect");
         }
-		else {
-		}
+        else {
+        }
     }
+    
+    const string authorizationEndpoint = "https://accounts.google.com/o/oauth2/v2/auth";
+    const string tokenEndpoint = "https://www.googleapis.com/oauth2/v4/token";
+    const string userInfoEndpoint = "https://www.googleapis.com/oauth2/v3/userinfo";
+    string clientID = "";
+    string clientSecret = "";
+
+    private void getClientInfo() {
+        FileStream fs = new FileStream(Application.dataPath+"/Secret/secret.txt", FileMode.Open);
+        StreamReader sr = new StreamReader(fs);
+        clientID = sr.ReadLine();
+        clientSecret = sr.ReadLine();
+        Debug.Log(clientID);
+        Debug.Log(clientSecret);
+        fs.Close();
+    }
+ 
+
+    public static int GetRandomUnusedPort() {
+        var listener = new TcpListener(IPAddress.Loopback, 0);
+        listener.Start();
+        var port = ((IPEndPoint)listener.LocalEndpoint).Port;
+        listener.Stop();
+        return port;
+    }
+    public async void GoogleLogin() { // Generates state and PKCE values.
+        string state = randomDataBase64url(32);
+        string code_verifier = randomDataBase64url(32);
+        string code_challenge = base64urlencodeNoPadding(sha256(code_verifier));
+        const string code_challenge_method = "S256";
+
+        // Creates a redirect URI using an available port on the loopback address.
+        string redirectURI = string.Format("http://{0}:{1}/", IPAddress.Loopback, GetRandomUnusedPort());
+        Debug.Log("redirect URI: " + redirectURI);
+
+        // Creates an HttpListener to listen for requests on that redirect URI.
+        var http = new HttpListener();
+        http.Prefixes.Add(redirectURI);
+        Debug.Log("Listening..");
+        http.Start();
+
+        getClientInfo();
+        // Creates the OAuth 2.0 authorization request.
+        string authorizationRequest = string.Format("{0}?response_type=code&scope=openid%20profile&redirect_uri={1}&client_id={2}&state={3}&code_challenge={4}&code_challenge_method={5}",
+            authorizationEndpoint,
+            System.Uri.EscapeDataString(redirectURI),
+            clientID,
+            state,
+            code_challenge,
+            code_challenge_method);
+
+        // Opens request in the browser.
+        System.Diagnostics.Process.Start(authorizationRequest);
+
+        // Waits for the OAuth authorization response.
+        var context = await http.GetContextAsync();
+
+        // Brings this app back to the foreground.
+
+        // Sends an HTTP response to the browser.
+        var response = context.Response;
+        string responseString = string.Format("<html><head><meta http-equiv='refresh' content='10;url=https://google.com'></head><body>Please return to the app.</body></html>");
+        var buffer = System.Text.Encoding.UTF8.GetBytes(responseString);
+        response.ContentLength64 = buffer.Length;
+        var responseOutput = response.OutputStream;
+        Task responseTask = responseOutput.WriteAsync(buffer, 0, buffer.Length).ContinueWith((task) =>
+        {
+            responseOutput.Close();
+            http.Stop();
+            Console.WriteLine("HTTP server stopped.");
+        });
+
+        // Checks for errors.
+        if (context.Request.QueryString.Get("error") != null) {
+            Debug.Log(String.Format("OAuth authorization error: {0}.", context.Request.QueryString.Get("error")));
+            return;
+        }
+        if (context.Request.QueryString.Get("code") == null
+            || context.Request.QueryString.Get("state") == null) {
+            Debug.Log("Malformed authorization response. " + context.Request.QueryString);
+            return;
+        }
+
+        // extracts the code
+        var code = context.Request.QueryString.Get("code");
+        var incoming_state = context.Request.QueryString.Get("state");
+
+        // Compares the receieved state to the expected value, to ensure that
+        // this app made the request which resulted in authorization.
+        if (incoming_state != state) {
+            Debug.Log(String.Format("Received request with invalid state ({0})", incoming_state));
+            return;
+        }
+        Debug.Log("Authorization code: " + code);
+
+        // Starts the code exchange at the Token Endpoint.
+        performCodeExchange(code, code_verifier, redirectURI);
+    }
+    async void performCodeExchange(string code, string code_verifier, string redirectURI) {
+        Debug.Log("Exchanging code for tokens...");
+
+        // builds the  request
+        string tokenRequestURI = "https://www.googleapis.com/oauth2/v4/token";
+        string tokenRequestBody = string.Format("code={0}&redirect_uri={1}&client_id={2}&code_verifier={3}&client_secret={4}&scope=&grant_type=authorization_code",
+            code,
+            System.Uri.EscapeDataString(redirectURI),
+            clientID,
+            code_verifier,
+            clientSecret
+            );
+
+        // sends the request
+        HttpWebRequest tokenRequest = (HttpWebRequest)WebRequest.Create(tokenRequestURI);
+        tokenRequest.Method = "POST";
+        tokenRequest.ContentType = "application/x-www-form-urlencoded";
+        tokenRequest.Accept = "Accept=text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8";
+        byte[] _byteVersion = Encoding.ASCII.GetBytes(tokenRequestBody);
+        tokenRequest.ContentLength = _byteVersion.Length;
+        Stream stream = tokenRequest.GetRequestStream();
+        await stream.WriteAsync(_byteVersion, 0, _byteVersion.Length);
+        stream.Close();
+
+        try {
+            // gets the response
+            WebResponse tokenResponse = await tokenRequest.GetResponseAsync();
+            using (StreamReader reader = new StreamReader(tokenResponse.GetResponseStream())) {
+                // reads response body
+                string responseText = await reader.ReadToEndAsync();
+                Debug.Log(responseText);
+
+                // converts to dictionary
+                Dictionary<string, string> tokenEndpointDecoded = JsonConvert.DeserializeObject<Dictionary<string, string>>(responseText);
+
+                string access_token = tokenEndpointDecoded["access_token"];
+                userinfoCall(access_token);
+            }
+        } catch (WebException ex) {
+            if (ex.Status == WebExceptionStatus.ProtocolError) {
+                var response = ex.Response as HttpWebResponse;
+                if (response != null) {
+                    Debug.Log("HTTP: " + response.StatusCode);
+                    using (StreamReader reader = new StreamReader(response.GetResponseStream())) {
+                        // reads response body
+                        string responseText = await reader.ReadToEndAsync();
+                        Debug.Log(responseText);
+                    }
+                }
+
+            }
+        }
+    }
+
+
+    async void userinfoCall(string access_token) {
+        Debug.Log("Making API Call to Userinfo...");
+
+        // builds the  request
+        string userinfoRequestURI = "https://www.googleapis.com/oauth2/v3/userinfo";
+
+        // sends the request
+        HttpWebRequest userinfoRequest = (HttpWebRequest)WebRequest.Create(userinfoRequestURI);
+        userinfoRequest.Method = "GET";
+        userinfoRequest.Headers.Add(string.Format("Authorization: Bearer {0}", access_token));
+        userinfoRequest.ContentType = "application/x-www-form-urlencoded";
+        userinfoRequest.Accept = "Accept=text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8";
+
+        // gets the response
+        WebResponse userinfoResponse = await userinfoRequest.GetResponseAsync();
+        using (StreamReader userinfoResponseReader = new StreamReader(userinfoResponse.GetResponseStream())) {
+            // reads response body
+            string userinfoResponseText = await userinfoResponseReader.ReadToEndAsync();
+            Debug.Log(userinfoResponseText);
+        }
+    }
+
+    /// <summary>
+    /// Appends the given string to the on-screen log, and the debug console.
+    /// </summary>
+    /// <param name="output">string to be appended</param>
+  
+
+    /// <summary>
+    /// Returns URI-safe data with a given input length.
+    /// </summary>
+    /// <param name="length">Input length (nb. output will be longer)</param>
+    /// <returns></returns>
+    public static string randomDataBase64url(uint length) {
+        RNGCryptoServiceProvider rng = new RNGCryptoServiceProvider();
+        byte[] bytes = new byte[length];
+        rng.GetBytes(bytes);
+        return base64urlencodeNoPadding(bytes);
+    }
+
+    /// <summary>
+    /// Returns the SHA256 hash of the input string.
+    /// </summary>
+    /// <param name="inputStirng"></param>
+    /// <returns></returns>
+    public static byte[] sha256(string inputStirng) {
+        byte[] bytes = Encoding.ASCII.GetBytes(inputStirng);
+        SHA256Managed sha256 = new SHA256Managed();
+        return sha256.ComputeHash(bytes);
+    }
+
+    /// <summary>
+    /// Base64url no-padding encodes the given input buffer.
+    /// </summary>
+    /// <param name="buffer"></param>
+    /// <returns></returns>
+    public static string base64urlencodeNoPadding(byte[] buffer) {
+        string base64 = Convert.ToBase64String(buffer);
+
+        // Converts base64 to base64url.
+        base64 = base64.Replace("+", "-");
+        base64 = base64.Replace("/", "_");
+        // Strips padding.
+        base64 = base64.Replace("=", "");
+
+        return base64;
+    }
+
 }
+
